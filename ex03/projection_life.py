727ad0d05326fffcea6ffb7dc58daf48af077bac
Pei <nuyiep@hotmail.com>
2023-10-01 16:28:39 +0800

    donee

--- ex03/projection_life.py
+++ ex03/projection_life.py
@@ -4,13 +4,13 @@ from load_csv import load
 
 
 def get_data(df_data, year):
-    '''Get data after zipped'''
+    '''Get data for year 1900'''
     try:
         year = int(year)
         x_data = df_data[str(year)].tolist()
     except Exception as err:
         print(err)
-        return (x_data)
+    return (x_data)
 
 
 def display_info(df_income, df_le):
@@ -22,7 +22,7 @@ def display_info(df_income, df_le):
         plt.xlabel("Gross Domestic Product")
         plt.ylabel("Life Expectancy")
         plt.title("1900")
-        plt.xscale("log")  # change the scale to logarithmic scale
+        plt.xscale("log")  # change the scale to logar
         plt.xticks([300, 1000, 10000], ['300', '1k', '10k'])
         plt.show()
     except Exception as err:
@@ -33,8 +33,8 @@ def display_info(df_income, df_le):
 
 def main():
     '''Main'''
-    df_income = load("../income_per_person \
-        _gdppercapita_ppp_inflation_adjusted.csv")
+    df_income = load(
+        "../income_per_person_gdppercapita_ppp_inflation_adjusted.csv")
     df_le = load("../life_expectancy_years.csv")
     display_info(df_income, df_le)
 
